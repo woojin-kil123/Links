c974dc2d90e6145cc5d3c7487b962f9e8bce4b8b
leesangminn <tkdals9909@naver.com>
2025-02-17 14:17:13 +0900

    멤버추가 커밋

--- src/main/java/kr/co/iei/member/controller/MemberController.java
+++ src/main/java/kr/co/iei/member/controller/MemberController.java
@@ -1,10 +1,19 @@
 package kr.co.iei.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.iei.member.model.service.MemberSerivce;
 
 @Controller
 @RequestMapping(value="/member")
 public class MemberController {
-
+ @Autowired
+ private MemberSerivce memberservice;
+ @GetMapping(value="/loginFrm")
+ public String  loginFrm() {
+	 return "member.login";
+ }
 }
--- src/main/java/kr/co/iei/member/vo/Member.java
+++ src/main/java/kr/co/iei/member/vo/Member.java
@@ -1,5 +1,20 @@
 package kr.co.iei.member.vo;
 
-public class Member {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Member {
+ private int memberNo;
+ private String memberName;
+ private String memberId;
+ private String memberPW;
+ private String memberEmail;
+ private String memberPhone;
+ private String memberRole;
+ private int memberLevel;
+ private String del;
 }
