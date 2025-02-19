f8e864b49337bb70d4e2ff0b8ba9a7c1a5583a02
woojin <dnwls9013@gmail.com>
2025-02-19 09:28:43 +0900

    2.18

--- src/main/java/kr/co/iei/webConfig.java
+++ src/main/java/kr/co/iei/webConfig.java
@@ -15,11 +15,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class webConfig implements WebMvcConfigurer{
 	@Value("${file.root}")
 	private String root;
-	
-	@Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
