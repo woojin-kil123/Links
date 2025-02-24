f1b42e981a5de62801fe4fca7a1cb80e4f142928
leesangminn <tkdals9909@naver.com>
2025-02-24 12:16:55 +0900

    1

--- src/main/java/kr/co/iei/member/controller/MemberController.java
+++ src/main/java/kr/co/iei/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package kr.co.iei.member.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -259,4 +261,5 @@ public String adminMsg(Model model) {
 	 int result= memberService.ajaxscoreNo(MemberId);
 	 return result;
  }
+ 
 }
--- src/main/java/kr/co/iei/member/model/service/MemberSerivce.java
+++ src/main/java/kr/co/iei/member/model/service/MemberSerivce.java
@@ -1,5 +1,7 @@
 package kr.co.iei.member.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -67,4 +69,5 @@ public class MemberSerivce {
 		return result;
 		
 	}
+	
 }
